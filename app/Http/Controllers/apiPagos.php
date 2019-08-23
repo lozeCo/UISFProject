d207b8049fcd35b5742fb5caea48767190ec56df
abn79-oax <abn79-oax>
2019-08-23 09:27:16 -0500

    agregando response para que se supere el problema de Origin

--- app/Http/Controllers/apiPagos.php
+++ app/Http/Controllers/apiPagos.php
@@ -12,13 +12,13 @@ class apiPagos extends Controller
     public function index($user,$password){
 
         if (\Auth::attempt(['email' => $user, 'password' => $password])) {
-            // Authentication passed...
+            // Authentication passed...import Button from './../../components/Button/Button';
             //return redirect()->intended('dashboard');
             // $sql = 'SELECT id FROM users WHERE email = "'.$user.'";';
             // $resp = DB::select($sql);
             // return $resp[0]->id;
             $id = \Auth::user()->id;
-            return $id;
+            return response($id)->header('Access-Control-Allow-Origin',"*");
         }
         return 0;
     }
